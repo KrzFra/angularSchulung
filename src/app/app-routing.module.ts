45bc4194e6fdccb0b15b03f930273fded52da3dd
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-03-21 16:42:44 +0100

    feat: basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,35 @@
+import { SeatSelectionComponent } from './views/seat-selection/seat-selection.component';
 import { ReservedSeatsComponent } from './views/reserved-seats/reserved-seats.component';
+import { LoginComponent } from './views/login/login.component';
+import { LocationComponent } from './views/location/location.component';
 import { ScheduleComponent } from './views/schedule/schedule.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{
-		path: 'select-seats',
-		component: ReservedSeatsComponent,
-	},
-	{
 		path: '',
 		component: ScheduleComponent,
+	},
+	{
+		path: 'schedule',
+		component: ScheduleComponent,
+	},
+	{
+		path: 'seat-selection',
+		component: SeatSelectionComponent,
+	},
+	{
+		path: 'location',
+		component: LocationComponent,
+	},
+	{
+		path: 'login',
+		component: LoginComponent,
+	},
+	{
+		path: 'reserved-seats',
+		component: ReservedSeatsComponent,
 	},
 	{
 		path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ScheduleComponent } from './views/schedule/schedule.component';
-import { SeatSelectionComponent } from './views/seat-selection/seat-selection.component';
-import { MakeReservationComponent } from './views/make-reservation/make-reservation.component';
 import { LocationComponent } from './views/location/location.component';
 import { LoginComponent } from './views/login/login.component';
+import { MakeReservationComponent } from './views/make-reservation/make-reservation.component';
 import { ReservedSeatsComponent } from './views/reserved-seats/reserved-seats.component';
+import { ScheduleComponent } from './views/schedule/schedule.component';
+import { SeatSelectionComponent } from './views/seat-selection/seat-selection.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		ScheduleComponent,
-		SeatSelectionComponent,
-		MakeReservationComponent,
 		LocationComponent,
 		LoginComponent,
+		MakeReservationComponent,
 		ReservedSeatsComponent,
+		ScheduleComponent,
+		SeatSelectionComponent,
 	],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
