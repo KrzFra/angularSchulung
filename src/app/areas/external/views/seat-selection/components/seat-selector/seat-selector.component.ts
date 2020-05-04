15e7dac83e4aaa715c0a6fdfd8eb702a755380b5
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-05-04 17:40:28 +0200

    feat: seat-selector seats: first steps

--- src/app/areas/external/views/seat-selection/components/seat-selector/seat-selector.component.ts
+++ src/app/areas/external/views/seat-selection/components/seat-selector/seat-selector.component.ts
@@ -12,5 +12,31 @@ export class SeatSelectorComponent implements OnInit {
 
 	@Input() theater: Theater;
 
-	ngOnInit(): void {}
+	rows: number[];
+	seatsInRows: number[];
+
+	maxTranslation = 50;
+
+	ngOnInit(): void {
+		this.rows = Array(this.theater.rows)
+			.fill(undefined)
+			.map((_, i) => i + 1);
+		this.seatsInRows = Array(this.theater.seatsInRows)
+			.fill(undefined)
+			.map((_, i) => i + 1);
+	}
+
+	getTranslationYFor(seatId: number): string {
+		console.log(seatId, this.theater.seatsInRows);
+
+		const x = (seatId - 1) / this.theater.seatsInRows;
+
+		const a = -4;
+		const b = 4;
+
+		const translationY = Math.ceil((a * x ** 2 + b * x) * this.maxTranslation);
+		console.log(x, a, b, translationY);
+
+		return `translateY(${translationY}px)`;
+	}
 }
--- src/app/areas/external/views/seat-selection/seat-selection.module.ts
+++ src/app/areas/external/views/seat-selection/seat-selection.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from '@core/core.module';
 import { SharedModule } from '@shared/shared.module';
 import { SeatSelectionSummaryComponent } from './components/seat-selection-summary/seat-selection-summary.component';
+import { SeatComponent } from './components/seat-selector/components/seat/seat.component';
 import { SeatSelectorComponent } from './components/seat-selector/seat-selector.component';
 import { SeatSelectionRoutingModule } from './seat-selection-routing.module';
 import { SeatSelectionComponent } from './view/seat-selection.component';
 
 @NgModule({
-	declarations: [SeatSelectionComponent, SeatSelectorComponent, SeatSelectionSummaryComponent],
+	declarations: [SeatComponent, SeatSelectionComponent, SeatSelectorComponent, SeatSelectionSummaryComponent],
 	imports: [CoreModule, CommonModule, SharedModule, SeatSelectionRoutingModule],
 })
 export class SeatSelectionModule {}
