fd979dc77b21c8912ddd688df25a3612f0688be9
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-05-06 17:47:34 +0200

    feat: header styling

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+	@HostBinding() class = 'app-header';
+
 	constructor() {}
 
 	ngOnInit(): void {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-	declarations: [MainMenuComponent, HeaderComponent],
+	declarations: [HeaderComponent],
 	imports: [CommonModule, RouterModule],
 	exports: [HeaderComponent],
 })
