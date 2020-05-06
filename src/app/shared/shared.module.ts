9c69997cd9d8852c7c2db64177eb3f07d63ebe42
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-05-06 18:59:10 +0200

    feat: seat-selection styling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,3 @@
-import { MovieSummaryLongComponent } from './components/movie-summary-long/movie-summary-long.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -6,8 +5,8 @@ import { CoreModule } from '@core/core.module';
 import { MovieSummaryComponent } from './components/movie-summary/movie-summary.component';
 
 @NgModule({
-	declarations: [MovieSummaryComponent, MovieSummaryLongComponent],
+	declarations: [MovieSummaryComponent],
 	imports: [CoreModule, CommonModule, RouterModule],
-	exports: [MovieSummaryComponent, MovieSummaryLongComponent],
+	exports: [MovieSummaryComponent],
 })
 export class SharedModule {}
