331e6d91f3734b7ecf8ea6ffd05dbb43fad3e3c1
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-07-01 10:56:17 +0200

    feat: letter pipes

--- src/app/areas/external/views/selections/selections.module.ts
+++ src/app/areas/external/views/selections/selections.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AppReservationsToStringModule } from '@core/pipes/reservations-to-string/reservations-to-string.module';
 import { AppMovieCardModule } from '@shared/components/movie-card/movie-card.module';
 import { SelectionsComponent } from './selections.component';
 import { AppSelectionsRoutingModule } from './selections.routing.module';
 
 @NgModule({
 	declarations: [SelectionsComponent],
-	imports: [AppMovieCardModule, AppSelectionsRoutingModule, CommonModule],
+	imports: [AppMovieCardModule, AppSelectionsRoutingModule, AppReservationsToStringModule, CommonModule],
 })
 export class AppSelectionsModule {}
